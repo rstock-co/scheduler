7e64667db3a8d452e792ff9349b76c69b2e6ef82
Richard Stock <rstock.co@gmail.com>
2022-10-03 16:23:44 -0600

    Add DayListItem component and initialize stories

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import "components/Button.scss";
+import "./Button.scss";
 
 const Button = props => {
   const buttonClass = classNames("button", {
