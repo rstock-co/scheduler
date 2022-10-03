a7eca0f0ab6f632d28e34ec948b6e9249655b974
Richard Stock <rstock.co@gmail.com>
2022-10-03 16:41:33 -0600

    Build and add conditional CSS styling to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,17 @@
 import React from "react";
+import classNames from "classnames";
+import "./DayListItem.scss";
 
 const DayListItem = props => {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 };
