56e3a367abffa3ff123c8f5116327e5d219d21e1
Richard Stock <rstock.co@gmail.com>
2022-10-03 14:48:22 -0600

    Add CSS styling and onClick/disable props to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,29 @@
 import React from "react";
-
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
-}
+const Button = props => {
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  const disabled = true;
+
+  const handleClick = () => console.log("button Clicked");
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
