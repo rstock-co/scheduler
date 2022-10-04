28eca12f0e7e4a6b003384de2f9198b455fabbcc
Richard Stock <rstock.co@gmail.com>
2022-10-04 16:51:39 -0600

    Build Cancel button click handler and test in Storybook

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,16 @@ const Form = ({ interviewers, onSave, onCancel, ...props }) => {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -27,7 +37,7 @@ const Form = ({ interviewers, onSave, onCancel, ...props }) => {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
           <Button confirm onClick={onSave}>
