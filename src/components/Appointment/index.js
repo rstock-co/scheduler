e82b9fbc87e700d964261e9f9b6c371473434f1c
Richard Stock <rstock.co@gmail.com>
2022-10-04 17:17:49 -0600

    Add Show and Empty functionality to Appointment component, plus story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "./styles.scss";
 
-const Appointment = () => {
-  return <article className="appointment"></article>;
+const Appointment = ({ id, time, interview, ...props }) => {
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {interview && <Show {...props} />}
+      {!interview && <Empty {...props} />}
+    </article>
+  );
 };
 
 export default Appointment;
