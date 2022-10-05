49fdceb6b549262326455abb470c2d9e6e131915
Richard Stock <rstock.co@gmail.com>
2022-10-05 16:39:33 -0600

    Combine all state into one object at application level

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,8 +44,14 @@ const appointments = {
 };
 
 const Application = () => {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments,
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days").then(response => {
@@ -64,7 +70,7 @@ const Application = () => {
           />
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
-            <DayList days={days} value={day} onChange={setDay} />
+            <DayList days={state.days} value={state.day} onChange={setDay} />
           </nav>
           <img
             className="sidebar__lhl sidebar--centered"
