eef8d7acd02fd6280583018a83f1eb39c5e653a7
Richard Stock <rstock.co@gmail.com>
2022-10-05 20:57:57 -0600

    Start working on custom hooks

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { DayList, Appointment } from "../components";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
-import axios from "axios";
 
 import "./Application.scss";
 
