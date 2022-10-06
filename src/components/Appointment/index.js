0f4fd651af0f29174d315b33c2c7e2b58383f7dc
Richard Stock <rstock.co@gmail.com>
2022-10-06 16:03:35 -0600

    Conditionally render Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss";
@@ -18,6 +19,7 @@ const Appointment = ({ time, interview, ...rest }) => {
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && <Show {...interview} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 };
