846ebbef35f5fbef419e3c232901c9a28df5495f
Richard Stock <rstock.co@gmail.com>
2022-10-05 20:22:15 -0600

    Refactor selector functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,21 +1,18 @@
 export const getAppointmentsForDay = (state, day) => {
   const days = state.days;
   const matchedDay = days.filter(d => d.name === day);
+
   if (days.length === 0 || matchedDay.length === 0) return [];
 
   const apptIDs = matchedDay[0].appointments;
-  const appts = Object.values(state.appointments).filter(appt =>
-    apptIDs.includes(appt.id)
-  );
-
+  const appts = apptIDs.map(appt => state.appointments[appt]);
   return appts;
 };
 
 export const getInterview = (state, interview) => {
   if (!interview) return null;
-  const result = { ...interview };
-  result.interviewer = Object.values(state.interviewers).filter(
-    i => interview.interviewer === i.id
-  )[0];
-  return result;
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer],
+  };
 };
