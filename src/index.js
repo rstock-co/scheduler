d28470bb7945a790421fdc5d7d8e808dd47c95d2
Richard Stock <rstock.co@gmail.com>
2022-10-12 16:23:35 -0600

    (Stretch) Add Netlify configuration file for serving static client assets for

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+/* eslint-disable import/first */
 import React from "react";
+import axios from "axios";
 import ReactDOM from "react-dom";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 import "index.scss";
 
