68df62dfb3b6c6239b09226cb8f92cc48cd2bb9d
Richard Stock <rstock.co@gmail.com>
2022-10-11 21:45:04 -0600

    Add 3rd integration test for Application component

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -90,4 +90,39 @@ describe("Application", () => {
 
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Find and Click the "Edit" button on the "Archie Cohen" appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    // 4. Change the sutdent's name and select the first interviewer
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Richard Stock" },
+    });
+    fireEvent.click(getByAltText(appointment, "Tori Malcolm"));
+
+    // 5. Click on the "Save" button
+    fireEvent.click(queryByText(appointment, "Save"));
+
+    // 6. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // 7. Wait until the element with the student's and interviewer's name is displayed.
+    await waitForElement(() => getByText(appointment, "Richard Stock"));
+    expect(getByText(appointment, "Tori Malcolm")).toBeInTheDocument();
+
+    // 8. Check that the DayListItem with the text "Monday" still has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
 });
